1ae8b64969a4805e01d85045a04549b014b79c5f
Rayan-Yazdan <122829101+Rayan-Yazdan@users.noreply.github.com>
2023-03-28 15:36:16 +0200

    Update webssh_project.sh
    change to root privilages

--- webssh_project.sh
+++ webssh_project.sh
@@ -21,12 +21,12 @@ echo -e "\e[37m"
 echo -e "\e[36m \e[5mDOWNLOADING PACKAGES \e[25m\e[37m"
 wget https://github.com/huashengdun/webssh/archive/refs/heads/master.zip
 unzip master.zip
-rm -r master.zip
+sudo rm -r master.zip
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mINSTALLING PACKAGES \e[25m\e[37m"
 echo -e "\e[32m INSTALL WEBSSH \e[37m"
 sudo pip install webssh
-rm -r webssh-master
+sudo rm -r webssh-master
 echo -e "\e[32m Done! \e[37m"
 echo -e "\e[32m LOCATING INSTALLATION PATH \e[37m"
 sudo python3 -m pip show webssh > installation-path.sh
@@ -37,91 +37,91 @@ sudo sed -i 's/Location: /export PIPATH=/g' installation-path.sh
 sudo chmod a+x installation-path.sh
 source installation-path.sh
 sudo ./installation-path.sh
-rm installation-path.sh
+sudo rm installation-path.sh
 cd $PIPATH/webssh/templates
 echo -e "\e[32m Done! \e[37m"
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mREMOVE CURRENT HTML FILE \e[25m\e[37m"
-rm index.html
+sudo rm index.html
 echo "index.html has been removed"
 echo -e "\e[36m \e[5mCREATE NEW HTML FILE \e[25m"
 echo -e "\e[37m"
-touch index.html
+sudo touch index.html
 echo "file created"
 echo -e "\e[36m \e[5mWRITING TO FILE \e[25m"
 echo -e "\e[37m"
-echo "<""!""DOCTYPE html>" >> index.html
-echo "<html lang=\"en\">" >> index.html
-echo   "<head>" >> index.html
-echo     "<meta charset=\"UTF-8\">" >> index.html
-echo     "<title> WebSSH </title>" >> index.html
-echo     "<link href=\"static/img/favicon.png\" rel=\"icon\" type=\"image/png\">" >> index.html
-echo     "<link href=\"static/css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" >> index.html
-echo     "<link href=\"static/css/xterm.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" >> index.html
-echo     "<link href=\"static/css/fullscreen.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" >> index.html
-echo     "<style>" >> index.html
-echo       ".row {" >> index.html
-echo         "margin-top: 15px;" >> index.html
-echo         "margin-bottom: 10px;" >> index.html
-echo       "}" >> index.html
-echo       ".container {" >> index.html
-echo         "margin-top: 20px;" >> index.html
-echo       "}" >> index.html
-echo       ".btn {" >> index.html
-echo         "margin-top: 15px;" >> index.html
-echo       "}" >> index.html
-echo       ".btn-danger {" >> index.html
-echo         "margin-left: 5px;" >> index.html
-echo       "}" >> index.html
-echo       "{% if font.family %}" >> index.html
-echo       "@font-face {" >> index.html
-echo         "font-family: '{{ font.family }}';" >> index.html
-echo         "src: url('{{ font.url }}');" >> index.html
-echo       "}" >> index.html
-echo       "body {" >> index.html
-echo         "font-family: '{{ font.family }}';" >> index.html
-echo       "}" >> index.html
-echo       "{% end %}" >> index.html
-echo     "</style>" >> index.html
-echo   "</head>" >> index.html
-echo   "<body>" >> index.html
-echo     "<div id=\"waiter\" style=\"display: none\"> Connecting ... </div>" >> index.html
-echo     "<div class=\"container form-container\" style=\"display: none\">" >> index.html
-echo       "<form id=\"connect\" action=\"\" method=\"post\" enctype=\"multipart/form-data\"{% if debug %} novalidate{% end %}>" >> index.html
-echo         "<div class=\"row\">" >> index.html
-echo           "<div class=\"col\">" >> index.html
-echo             "<label for=\"Hostname\">Hostname</label>" >> index.html
-echo             "<input class=\"form-control\" type=\"text\" id=\"hostname\" name=\"hostname\" placeholder=\"$HOSTNAME\" value=\"$HOSTNAME\" readonly>" >> index.html
-echo           "</div>" >> index.html
-echo         "</div>" >> index.html
-echo         "<div class=\"row\">" >> index.html
-echo           "<div class=\"col\">" >> index.html
-echo             "<label for=\"Username\">Username</label>" >> index.html
-echo             "<input class=\"form-control\" type=\"text\" id=\"username\" name=\"username\" placeholder=\"st$MYDATE\"value=\"st$MYDATE\" required>" >> index.html
-echo           "</div>" >> index.html
-echo           "<div class=\"col\">" >> index.html
-echo             "<label for=\"Password\">Password</label>" >> index.html
-echo             "<input class=\"form-control\" type=\"password\" id=\"password\" name=\"password\" value=\"\">" >> index.html
-echo           "</div>" >> index.html
-echo         "</div>" >> index.html
-echo         "<input type=\"hidden\" id=\"term\" name=\"term\" value=\"xterm-256color\">" >> index.html
-echo         "{% module xsrf_form_html() %}" >> index.html
-echo         "<button type=\"submit\" class=\"btn btn-primary\">Connect</button>" >> index.html
-echo         "<button type=\"reset\" class=\"btn btn-danger\">Reset</button>" >> index.html
-echo       "</form>" >> index.html
-echo     "</div>" >> index.html
-echo     "<div class=\"container\">" >> index.html
-echo       "<div id=\"status\" style=\"color: red;\"></div>" >> index.html
-echo       "<div id=\"terminal\"></div>" >> index.html
-echo     "</div>" >> index.html
-echo     "<script src=\"static/js/jquery.min.js\"></script>" >> index.html
-echo     "<script src=\"static/js/popper.min.js\"></script>" >> index.html
-echo     "<script src=\"static/js/bootstrap.min.js\"></script>" >> index.html
-echo     "<script src=\"static/js/xterm.min.js\"></script>" >> index.html
-echo     "<script src=\"static/js/xterm-addon-fit.min.js\"></script>" >> index.html
-echo     "<script src=\"static/js/main.js\"></script>" >> index.html
-echo   "</body>" >> index.html
-echo "</html>" >> index.html
+sudo echo "<""!""DOCTYPE html>" >> index.html
+sudo echo "<html lang=\"en\">" >> index.html
+sudo echo   "<head>" >> index.html
+sudo echo     "<meta charset=\"UTF-8\">" >> index.html
+sudo echo     "<title> WebSSH </title>" >> index.html
+sudo echo     "<link href=\"static/img/favicon.png\" rel=\"icon\" type=\"image/png\">" >> index.html
+sudo echo     "<link href=\"static/css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" >> index.html
+sudo echo     "<link href=\"static/css/xterm.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" >> index.html
+sudo echo     "<link href=\"static/css/fullscreen.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" >> index.html
+sudo echo     "<style>" >> index.html
+sudo echo       ".row {" >> index.html
+sudo echo         "margin-top: 15px;" >> index.html
+sudo echo         "margin-bottom: 10px;" >> index.html
+sudo echo       "}" >> index.html
+sudo echo       ".container {" >> index.html
+sudo echo         "margin-top: 20px;" >> index.html
+sudo echo       "}" >> index.html
+sudo echo       ".btn {" >> index.html
+sudo echo         "margin-top: 15px;" >> index.html
+sudo echo       "}" >> index.html
+sudo echo       ".btn-danger {" >> index.html
+sudo echo         "margin-left: 5px;" >> index.html
+sudo echo       "}" >> index.html
+sudo echo       "{% if font.family %}" >> index.html
+sudo echo       "@font-face {" >> index.html
+sudo echo         "font-family: '{{ font.family }}';" >> index.html
+sudo echo         "src: url('{{ font.url }}');" >> index.html
+sudo echo       "}" >> index.html
+sudo echo       "body {" >> index.html
+sudo echo         "font-family: '{{ font.family }}';" >> index.html
+sudo echo       "}" >> index.html
+sudo echo       "{% end %}" >> index.html
+sudo echo     "</style>" >> index.html
+sudo echo   "</head>" >> index.html
+sudo echo   "<body>" >> index.html
+sudo echo     "<div id=\"waiter\" style=\"display: none\"> Connecting ... </div>" >> index.html
+sudo echo     "<div class=\"container form-container\" style=\"display: none\">" >> index.html
+sudo echo       "<form id=\"connect\" action=\"\" method=\"post\" enctype=\"multipart/form-data\"{% if debug %} novalidate{% end %}>" >> index.html
+sudo echo         "<div class=\"row\">" >> index.html
+sudo echo           "<div class=\"col\">" >> index.html
+sudo echo             "<label for=\"Hostname\">Hostname</label>" >> index.html
+sudo echo             "<input class=\"form-control\" type=\"text\" id=\"hostname\" name=\"hostname\" placeholder=\"$HOSTNAME\" value=\"$HOSTNAME\" readonly>" >> index.html
+sudo echo           "</div>" >> index.html
+sudo echo         "</div>" >> index.html
+sudo echo         "<div class=\"row\">" >> index.html
+sudo echo           "<div class=\"col\">" >> index.html
+sudo echo             "<label for=\"Username\">Username</label>" >> index.html
+sudo echo             "<input class=\"form-control\" type=\"text\" id=\"username\" name=\"username\" placeholder=\"st$MYDATE\"value=\"st$MYDATE\" required>" >> index.html
+sudo echo           "</div>" >> index.html
+sudo echo           "<div class=\"col\">" >> index.html
+sudo echo             "<label for=\"Password\">Password</label>" >> index.html
+sudo echo             "<input class=\"form-control\" type=\"password\" id=\"password\" name=\"password\" value=\"\">" >> index.html
+sudo echo           "</div>" >> index.html
+sudo echo         "</div>" >> index.html
+sudo echo         "<input type=\"hidden\" id=\"term\" name=\"term\" value=\"xterm-256color\">" >> index.html
+sudo echo         "{% module xsrf_form_html() %}" >> index.html
+sudo echo         "<button type=\"submit\" class=\"btn btn-primary\">Connect</button>" >> index.html
+sudo echo         "<button type=\"reset\" class=\"btn btn-danger\">Reset</button>" >> index.html
+sudo echo       "</form>" >> index.html
+sudo echo     "</div>" >> index.html
+sudo echo     "<div class=\"container\">" >> index.html
+sudo echo       "<div id=\"status\" style=\"color: red;\"></div>" >> index.html
+sudo echo       "<div id=\"terminal\"></div>" >> index.html
+sudo echo     "</div>" >> index.html
+sudo echo     "<script src=\"static/js/jquery.min.js\"></script>" >> index.html
+sudo echo     "<script src=\"static/js/popper.min.js\"></script>" >> index.html
+sudo echo     "<script src=\"static/js/bootstrap.min.js\"></script>" >> index.html
+sudo echo     "<script src=\"static/js/xterm.min.js\"></script>" >> index.html
+sudo echo     "<script src=\"static/js/xterm-addon-fit.min.js\"></script>" >> index.html
+sudo echo     "<script src=\"static/js/main.js\"></script>" >> index.html
+sudo echo   "</body>" >> index.html
+sudo echo "</html>" >> index.html
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mCOPY WSSH EXECUTABLE FROM ~/.local/bin TO /usr/local/bin \e[25m\e[37m"
 cd ~/.local/bin
