0384c136c58e2dec24c7bc383f26b5cea3f90758
Rayan-Yazdan <122829101+Rayan-Yazdan@users.noreply.github.com>
2023-03-28 15:15:34 +0200

    Update webssh_project.sh
    change to root privilages

--- webssh_project.sh
+++ webssh_project.sh
@@ -29,7 +29,7 @@ sudo pip install webssh
 rm -r webssh-master
 echo -e "\e[32m Done! \e[37m"
 echo -e "\e[32m LOCATING INSTALLATION PATH \e[37m"
-python3 -m pip show webssh > installation-path.sh
+sudo python3 -m pip show webssh > installation-path.sh
 sudo sed -i '1,7d;9d;$d' installation-path.sh
 echo -e "\e[32m Done! \e[37m"
 echo -e "\e[32m MAKING AN ENVIRONMENT PATH \e[37m"
