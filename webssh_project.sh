d8f88da4c0db5dac31602ce1f2b0c55b9449cb67
Rayan-Yazdan <122829101+Rayan-Yazdan@users.noreply.github.com>
2023-04-03 14:00:44 +0200

    Update webssh_project.sh

--- webssh_project.sh
+++ webssh_project.sh
@@ -1,6 +1,9 @@
+#Updates System:
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mUPDATE SYSTEM \e[25m\e[37m"
 sudo apt-get update
+
+#Installs the commands we need during the code execution:
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mINSTALLING NECESSARY COMMANDS \e[25m\e[37m"
 echo -e "\e[32m PIP COMMAND \e[37m"
@@ -13,20 +16,28 @@ echo -e "\e[32m SED COMMAND \e[37m"
 sudo apt install sed
 echo -e "\e[32m SOURCE COMMAND \e[37m"
 sudo apt install source
+
+#Adds the current date to a shell variable, further to be used in index.html:
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mINSTALLING SHELL VARIABLES \e[25m\e[37m"
 MYDATE=$(date +%Y)_
 echo "current 4-digit-year command is set to MYDATE"
+
+#Downloads and unzips the webssh package from gihub:
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mDOWNLOADING PACKAGES \e[25m\e[37m"
 sudo wget https://github.com/huashengdun/webssh/archive/refs/heads/master.zip
 sudo unzip master.zip
 sudo rm -r master.zip
+
+#Installs webssh package and then deletes the installation files
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mINSTALLING PACKAGES \e[25m\e[37m"
 echo -e "\e[32m INSTALL WEBSSH \e[37m"
 sudo pip install webssh
 sudo rm -r webssh-master
+
+#Locates webssh installation path, put it in a .sh file to make an Environment path (PIPATH)
 echo -e "\e[32m Done! \e[37m"
 echo -e "\e[32m LOCATING INSTALLATION PATH \e[37m"
 sudo python3 -m pip show webssh > installation-path.sh
@@ -40,6 +51,8 @@ sudo ./installation-path.sh
 sudo rm installation-path.sh
 cd $PIPATH/webssh/templates
 echo -e "\e[32m Done! \e[37m"
+
+#Removes current index.html file, writes the new code to it
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mREMOVE CURRENT HTML FILE \e[25m\e[37m"
 sudo rm index.html
@@ -50,77 +63,77 @@ sudo touch index.html
 echo "file created"
 echo -e "\e[36m \e[5mWRITING TO FILE \e[25m"
 echo -e "\e[37m"
-sudo echo "<""!""DOCTYPE html>" | sudo tee -a index.html
-sudo echo "<html lang=\"en\">" | sudo tee -a index.html
-sudo echo   "<head>" | sudo tee -a index.html
-sudo echo     "<meta charset=\"UTF-8\">" | sudo tee -a index.html
-sudo echo     "<title> WebSSH </title>" | sudo tee -a index.html
-sudo echo     "<link href=\"static/img/favicon.png\" rel=\"icon\" type=\"image/png\">" | sudo tee -a index.html
-sudo echo     "<link href=\"static/css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" | sudo tee -a index.html
-sudo echo     "<link href=\"static/css/xterm.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" | sudo tee -a index.html
-sudo echo     "<link href=\"static/css/fullscreen.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" | sudo tee -a index.html
-sudo echo     "<style>" | sudo tee -a index.html
-sudo echo       ".row {" | sudo tee -a index.html
-sudo echo         "margin-top: 15px;" | sudo tee -a index.html
-sudo echo         "margin-bottom: 10px;" | sudo tee -a index.html
-sudo echo       "}" | sudo tee -a index.html
-sudo echo       ".container {" | sudo tee -a index.html
-sudo echo         "margin-top: 20px;" | sudo tee -a index.html
-sudo echo       "}" | sudo tee -a index.html
-sudo echo       ".btn {" | sudo tee -a index.html
-sudo echo         "margin-top: 15px;" | sudo tee -a index.html
-sudo echo       "}" | sudo tee -a index.html
-sudo echo       ".btn-danger {" | sudo tee -a index.html
-sudo echo         "margin-left: 5px;" | sudo tee -a index.html
-sudo echo       "}" | sudo tee -a index.html
-sudo echo       "{% if font.family %}" | sudo tee -a index.html
-sudo echo       "@font-face {" | sudo tee -a index.html
-sudo echo         "font-family: '{{ font.family }}';" | sudo tee -a index.html
-sudo echo         "src: url('{{ font.url }}');" | sudo tee -a index.html
-sudo echo       "}" | sudo tee -a index.html
-sudo echo       "body {" | sudo tee -a index.html
-sudo echo         "font-family: '{{ font.family }}';" | sudo tee -a index.html
-sudo echo       "}" | sudo tee -a index.html
-sudo echo       "{% end %}" | sudo tee -a index.html
-sudo echo     "</style>" | sudo tee -a index.html
-sudo echo   "</head>" | sudo tee -a index.html
-sudo echo   "<body>" | sudo tee -a index.html
-sudo echo     "<div id=\"waiter\" style=\"display: none\"> Connecting ... </div>" | sudo tee -a index.html
-sudo echo     "<div class=\"container form-container\" style=\"display: none\">" | sudo tee -a index.html
-sudo echo       "<form id=\"connect\" action=\"\" method=\"post\" enctype=\"multipart/form-data\"{% if debug %} novalidate{% end %}>" | sudo tee -a index.html
-sudo echo         "<div class=\"row\">" | sudo tee -a index.html
-sudo echo           "<div class=\"col\">" | sudo tee -a index.html
-sudo echo             "<label for=\"Hostname\">Hostname</label>" | sudo tee -a index.html
-sudo echo             "<input class=\"form-control\" type=\"text\" id=\"hostname\" name=\"hostname\" placeholder=\"$HOSTNAME\" value=\"$HOSTNAME\" readonly>" | sudo tee -a index.html
-sudo echo           "</div>" | sudo tee -a index.html
-sudo echo         "</div>" | sudo tee -a index.html
-sudo echo         "<div class=\"row\">" | sudo tee -a index.html
-sudo echo           "<div class=\"col\">" | sudo tee -a index.html
-sudo echo             "<label for=\"Username\">Username</label>" | sudo tee -a index.html
-sudo echo             "<input class=\"form-control\" type=\"text\" id=\"username\" name=\"username\" placeholder=\"st$MYDATE\"value=\"st$MYDATE\" required>" | sudo tee -a index.html
-sudo echo           "</div>" | sudo tee -a index.html
-sudo echo           "<div class=\"col\">" | sudo tee -a index.html
-sudo echo             "<label for=\"Password\">Password</label>" | sudo tee -a index.html
-sudo echo             "<input class=\"form-control\" type=\"password\" id=\"password\" name=\"password\" value=\"\">" | sudo tee -a index.html
-sudo echo           "</div>" | sudo tee -a index.html
-sudo echo         "</div>" | sudo tee -a index.html
-sudo echo         "<input type=\"hidden\" id=\"term\" name=\"term\" value=\"xterm-256color\">" | sudo tee -a index.html
-sudo echo         "{% module xsrf_form_html() %}" | sudo tee -a index.html
-sudo echo         "<button type=\"submit\" class=\"btn btn-primary\">Connect</button>" | sudo tee -a index.html
-sudo echo         "<button type=\"reset\" class=\"btn btn-danger\">Reset</button>" | sudo tee -a index.html
-sudo echo       "</form>" | sudo tee -a index.html
-sudo echo     "</div>" | sudo tee -a index.html
-sudo echo     "<div class=\"container\">" | sudo tee -a index.html
-sudo echo       "<div id=\"status\" style=\"color: red;\"></div>" | sudo tee -a index.html
-sudo echo       "<div id=\"terminal\"></div>" | sudo tee -a index.html
-sudo echo     "</div>" | sudo tee -a index.html
-sudo echo     "<script src=\"static/js/jquery.min.js\"></script>" | sudo tee -a index.html
-sudo echo     "<script src=\"static/js/popper.min.js\"></script>" | sudo tee -a index.html
-sudo echo     "<script src=\"static/js/bootstrap.min.js\"></script>" | sudo tee -a index.html
-sudo echo     "<script src=\"static/js/xterm.min.js\"></script>" | sudo tee -a index.html
-sudo echo     "<script src=\"static/js/xterm-addon-fit.min.js\"></script>" | sudo tee -a index.html
-sudo echo     "<script src=\"static/js/main.js\"></script>" | sudo tee -a index.html
-sudo echo   "</body>" | sudo tee -a index.html
+sudo echo "<""!""DOCTYPE html>"                                     | sudo tee -a index.html
+sudo echo "<html lang=\"en\">"                                      | sudo tee -a index.html
+sudo echo "  <head>"                                                | sudo tee -a index.html
+sudo echo "    <meta charset=\"UTF-8\">"                            | sudo tee -a index.html
+sudo echo "    <title> WebSSH </title>"                             | sudo tee -a index.html
+sudo echo "    <link href=\"static/img/favicon.png\" rel=\"icon\" type=\"image/png\">" | sudo tee -a index.html
+sudo echo "    <link href=\"static/css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" | sudo tee -a index.html
+sudo echo "    <link href=\"static/css/xterm.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" | sudo tee -a index.html
+sudo echo "    <link href=\"static/css/fullscreen.min.css\" rel=\"stylesheet\" type=\"text/css\"/>" | sudo tee -a index.html
+sudo echo "    <style>" | sudo tee -a index.html
+sudo echo "      .row {" | sudo tee -a index.html
+sudo echo "        margin-top: 15px;" | sudo tee -a index.html
+sudo echo "        margin-bottom: 10px;" | sudo tee -a index.html
+sudo echo "      }" | sudo tee -a index.html
+sudo echo "      .container {" | sudo tee -a index.html
+sudo echo "        margin-top: 20px;" | sudo tee -a index.html
+sudo echo "      }" | sudo tee -a index.html
+sudo echo "      .btn {" | sudo tee -a index.html
+sudo echo "        margin-top: 15px;" | sudo tee -a index.html
+sudo echo "      }" | sudo tee -a index.html
+sudo echo "      .btn-danger {" | sudo tee -a index.html
+sudo echo "        margin-left: 5px;" | sudo tee -a index.html
+sudo echo "      }" | sudo tee -a index.html
+sudo echo "      {% if font.family %}" | sudo tee -a index.html
+sudo echo "      @font-face {" | sudo tee -a index.html
+sudo echo "        font-family: '{{ font.family }}';" | sudo tee -a index.html
+sudo echo "        src: url('{{ font.url }}');" | sudo tee -a index.html
+sudo echo "      }" | sudo tee -a index.html
+sudo echo "      body {" | sudo tee -a index.html
+sudo echo "        font-family: '{{ font.family }}';" | sudo tee -a index.html
+sudo echo "      }" | sudo tee -a index.html
+sudo echo "      {% end %}" | sudo tee -a index.html
+sudo echo "    </style>" | sudo tee -a index.html
+sudo echo "  </head>" | sudo tee -a index.html
+sudo echo "  <body>" | sudo tee -a index.html
+sudo echo "    <div id=\"waiter\" style=\"display: none\"> Connecting ... </div>" | sudo tee -a index.html
+sudo echo "    <div class=\"container form-container\" style=\"display: none\">" | sudo tee -a index.html
+sudo echo "      <form id=\"connect\" action=\"\" method=\"post\" enctype=\"multipart/form-data\"{% if debug %} novalidate{% end %}>" | sudo tee -a index.html
+sudo echo "        <div class=\"row\">" | sudo tee -a index.html
+sudo echo "          <div class=\"col\">" | sudo tee -a index.html
+sudo echo "            <label for=\"Hostname\">Hostname</label>" | sudo tee -a index.html
+sudo echo "            <input class=\"form-control\" type=\"text\" id=\"hostname\" name=\"hostname\" placeholder=\"$HOSTNAME\" value=\"$HOSTNAME\" readonly>" | sudo tee -a index.html
+sudo echo "          </div>" | sudo tee -a index.html
+sudo echo "        </div>" | sudo tee -a index.html
+sudo echo "        <div class=\"row\">" | sudo tee -a index.html
+sudo echo "          <div class=\"col\">" | sudo tee -a index.html
+sudo echo "            <label for=\"Username\">Username</label>" | sudo tee -a index.html
+sudo echo "            <input class=\"form-control\" type=\"text\" id=\"username\" name=\"username\" placeholder=\"st$MYDATE\"value=\"st$MYDATE\" required>" | sudo tee -a index.html
+sudo echo "          </div>" | sudo tee -a index.html
+sudo echo "          <div class=\"col\">" | sudo tee -a index.html
+sudo echo "            <label for=\"Password\">Password</label>" | sudo tee -a index.html
+sudo echo "            <input class=\"form-control\" type=\"password\" id=\"password\" name=\"password\" value=\"\">" | sudo tee -a index.html
+sudo echo "          </div>" | sudo tee -a index.html
+sudo echo "        </div>" | sudo tee -a index.html
+sudo echo "        <input type=\"hidden\" id=\"term\" name=\"term\" value=\"xterm-256color\">" | sudo tee -a index.html
+sudo echo "        {% module xsrf_form_html() %}" | sudo tee -a index.html
+sudo echo "        <button type=\"submit\" class=\"btn btn-primary\">Connect</button>" | sudo tee -a index.html
+sudo echo "        <button type=\"reset\" class=\"btn btn-danger\">Reset</button>" | sudo tee -a index.html
+sudo echo "      </form>" | sudo tee -a index.html
+sudo echo "    </div>" | sudo tee -a index.html
+sudo echo "    <div class=\"container\">" | sudo tee -a index.html
+sudo echo "      <div id=\"status\" style=\"color: red;\"></div>" | sudo tee -a index.html
+sudo echo "      <div id=\"terminal\"></div>" | sudo tee -a index.html
+sudo echo "    </div>" | sudo tee -a index.html
+sudo echo "    <script src=\"static/js/jquery.min.js\"></script>" | sudo tee -a index.html
+sudo echo "    <script src=\"static/js/popper.min.js\"></script>" | sudo tee -a index.html
+sudo echo "    <script src=\"static/js/bootstrap.min.js\"></script>" | sudo tee -a index.html
+sudo echo "    <script src=\"static/js/xterm.min.js\"></script>" | sudo tee -a index.html
+sudo echo "    <script src=\"static/js/xterm-addon-fit.min.js\"></script>" | sudo tee -a index.html
+sudo echo "    <script src=\"static/js/main.js\"></script>" | sudo tee -a index.html
+sudo echo "  </body>" | sudo tee -a index.html
 sudo echo "</html>" | sudo tee -a index.html
 
 echo -e "\e[37m"
