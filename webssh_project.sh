148637b1883da5c481c7e342c5df890f4a912c1a
Rayan-Yazdan <122829101+Rayan-Yazdan@users.noreply.github.com>
2023-04-03 23:00:42 +0200

    Update webssh_project.sh

--- webssh_project.sh
+++ webssh_project.sh
@@ -2,6 +2,7 @@
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mUPDATE SYSTEM \e[25m\e[37m"
 sudo apt-get update
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Installs the commands we need during the code execution:
 echo -e "\e[37m"
@@ -16,12 +17,14 @@ echo -e "\e[32m SED COMMAND \e[37m"
 sudo apt install sed
 echo -e "\e[32m SOURCE COMMAND \e[37m"
 sudo apt install source
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Adds the current date to a shell variable, further to be used in index.html:
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mINSTALLING SHELL VARIABLES \e[25m\e[37m"
 MYDATE=$(date +%Y)_
 echo "current 4-digit-year command is set to MYDATE"
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Downloads and unzips the webssh package from gihub:
 echo -e "\e[37m"
@@ -29,6 +32,7 @@ echo -e "\e[36m \e[5mDOWNLOADING PACKAGES \e[25m\e[37m"
 sudo wget https://github.com/huashengdun/webssh/archive/refs/heads/master.zip
 sudo unzip master.zip
 sudo rm -r master.zip
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Installs webssh package and then deletes the installation files
 echo -e "\e[37m"
@@ -36,13 +40,14 @@ echo -e "\e[36m \e[5mINSTALLING PACKAGES \e[25m\e[37m"
 echo -e "\e[32m INSTALL WEBSSH \e[37m"
 sudo pip install webssh
 sudo rm -r webssh-master
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Locates webssh installation path, put it in a .sh file to make an Environment path (PIPATH)
-echo -e "\e[32m Done! \e[37m"
+echo -e "\e[37m"
 echo -e "\e[32m LOCATING INSTALLATION PATH \e[37m"
 sudo python3 -m pip show webssh > installation-path.sh
 sudo sed -i '1,7d;9d;$d' installation-path.sh
-echo -e "\e[32m Done! \e[37m"
+echo -e "\e[37m"
 echo -e "\e[32m MAKING AN ENVIRONMENT PATH \e[37m"
 sudo sed -i 's/Location: /export PIPATH=/g' installation-path.sh
 sudo chmod a+x installation-path.sh
@@ -50,7 +55,7 @@ source installation-path.sh
 sudo ./installation-path.sh
 sudo rm installation-path.sh
 cd $PIPATH/webssh/templates
-echo -e "\e[32m Done! \e[37m"
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Removes current index.html file, creates new one, writes the new code to it
 echo -e "\e[37m"
@@ -135,12 +140,14 @@ sudo echo "    <script src=\"static/js/xterm-addon-fit.min.js\"></script>"      
 sudo echo "    <script src=\"static/js/main.js\"></script>"                     | sudo tee -a index.html
 sudo echo "  </body>"                                                           | sudo tee -a index.html
 sudo echo "</html>"                                                             | sudo tee -a index.html
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Writing an alias command to run Uni-Siegen Server with the specific cert and key file with the shortcut command (wsshusi)
 echo -e "\e[37m"
 echo -e "\e[36m \e[5mCreating an Alias Command (wsshusi) for our wssh key and cert file \e[25m\e[37m"
 alias wsshusi='sudo wssh --certfile=/etc/letsencrypt/live/ubi21.informatik.uni-siegen.de/cert.pem --keyfile=/etc/letsencrypt/live/ubi21.informatik.uni-siegen.de/privkey.pem --sslport=8888 --port=4433'
 echo "Specific Command to run Uni-Siegen Web SSH Server is: wsshusi"
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 #Make an startup file in init.d and write to it
 echo -e "\e[37m"
@@ -188,6 +195,7 @@ sudo echo "       echo \"Usage: \$0 {start|stop|status|restart}\""	| sudo tee -a
 sudo echo "esac"							| sudo tee -a startup-run.sh
 sudo echo ""								| sudo tee -a startup-run.sh
 sudo echo "exit 0" 							| sudo tee -a startup-run.sh
+echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 
 
 
