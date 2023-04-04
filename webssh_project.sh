3fb78c2b53c38e45a6f3ed717716ae34188e8a8a
Rayan-Yazdan <122829101+Rayan-Yazdan@users.noreply.github.com>
2023-04-04 14:06:23 +0200

    Update webssh_project.sh

--- webssh_project.sh
+++ webssh_project.sh
@@ -211,8 +211,3 @@ echo "FINISHED, SCROLL UP TO SEE IF THERE IS AN ERROR ON THE PROCESS"
 echo "Making file executable:"
 sudo chmod a+x webssh
 echo "done!"
-
-#sudo echo "sudo wssh --certfile=/etc/letsencrypt/live/ubi21.informatik.uni-siegen.de/cert.pem --keyfile=/etc/letsencrypt/live/ubi21.informatik.uni-siegen.de/privkey.pem --sslport=8888 --port=4433" | sudo tee -a webssh
-
-
-
