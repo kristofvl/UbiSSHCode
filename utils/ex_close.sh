4b09c94424a5748c7f25b7a98b72ea4bf15dca38
kristofvl <kvl@eti.uni-siegen.de>
2024-12-18 11:25:58 +0100

    added closing of assignments

---
+++ utils/ex_close.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# first check if user is running as root:
+if [[ $EUID > 0 ]]
+    then echo "please run as root"
+    exit
+fi
+
+if [ $# -eq 0 ] ; then
+    echo -e 'usage: '$0' <year> <ex> <list.txt>'
+    exit 1
+fi
+
+# for counting all errors/warnings:
+ERRCOUNT=0
+# arguments
+ex=$2
+CSV=$3
+
+while IFS=',' read -r f1 f2
+do
+	pkill -u ${f1:10:22}
+done < "$CSV"
+
+for f in /home/st$1_*; do
+	if [ -d $f ]; then
+      st_dir=${f##*/}
+    	if [ -d $f/$ex ]; then  # if directory exists:
+        mkdir -p /home/tutor/jplag/exercises/$ex/$st_dir
+        cp -r -u $f/$ex/*.cpp /home/tutor/jplag/exercises/$ex/$st_dir/
+		  # change ownership of all expired exercises:
+        sudo chown -R tutor:tutor /home/$st_dir/$ex/
+        sudo chmod a-w /home/$st_dir/$ex/
+        sudo chmod a-w /home/$st_dir/$ex/*.cpp
+        sudo chmod a-w /home/$st_dir/$ex/*.h
+      else  # if directory doesn't exist:
+        # make an unchangeable directory to prevent starting new solutions after deadline
+        sudo mkdir /home/$st_dir/$ex/
+        sudo chmod a-w /home/$st_dir/$ex/
+      fi
+	fi
+done
